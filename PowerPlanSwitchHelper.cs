ef7f3fbd453fcc20d2aff1c3ff9de4393f98faaa
Amit Patankar <amit.patankar@gmail.com>
2014-01-07 09:24:03 -0600

    Initial commit.

---
+++ PowerPlanSwitchHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PowerPlanSwitch
+{
+    public static class PowerPlanSwitchHelper
+    {
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerSetActiveScheme")]
+        public static extern uint PowerSetActiveScheme(IntPtr UserPowerKey, ref Guid ActivePolicyGuid);
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerGetActiveScheme")]
+        public static extern uint PowerGetActiveScheme(IntPtr UserPowerKey, out IntPtr ActivePolicyGuid);
+
+        [DllImportAttribute("powrprof.dll", EntryPoint = "PowerReadFriendlyName")]
+        public static extern uint PowerReadFriendlyName(IntPtr RootPowerKey, ref Guid SchemeGuid, IntPtr SubGroupOfPowerSettingsGuid, IntPtr PowerSettingGuid, IntPtr Buffer, ref uint BufferSize);
+
+        private static Dictionary<string, string> powerPlans;
+        
+        private static PowerPlanSwitchHelper()
+        {
+            powerPlans=new Dictionary<string,string>();
+            powerPlans.Add("MAX_BATTERY","a1841308-3541-4fab-bc81-f71556f20b4a");
+            powerPlans.Add("MAX_PERFORMANCE","8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c");
+            powerPlans.Add("BALANCED","381b4222-f694-41f0-9685-ff5bb260df2e");
+        }
+
+
+        private static string GetPowerPlanName(Guid guid)
+        {
+            string name = string.Empty;
+            IntPtr lpszName = (IntPtr)null;
+            uint dwSize = 0;
+
+            PowerReadFriendlyName((IntPtr)null, ref guid, (IntPtr)null, (IntPtr)null, lpszName, ref dwSize);
+            if (dwSize > 0)
+            {
+                lpszName = Marshal.AllocHGlobal((int)dwSize);
+                if (0 == PowerReadFriendlyName((IntPtr)null, ref guid, (IntPtr)null, (IntPtr)null, lpszName, ref dwSize))
+                {
+                    name = Marshal.PtrToStringUni(lpszName);
+                }
+                if (lpszName != IntPtr.Zero)
+                    Marshal.FreeHGlobal(lpszName);
+            }
+            return name;
+        }
+    }
+}
---
+++ SwitchService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerPlanSwitch
+{
+    public partial class SwitchService : ServiceBase
+    {
+
+        public SwitchService()
+        {
+            InitializeComponent();
+        }
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            switch (powerStatus)
+            {
+                case PowerBroadcastStatus.PowerStatusChange:
+                    SetPowerPlanSettingOnSystemByConnectedPowerStatus();
+                    break;
+                case PowerBroadcastStatus.BatteryLow:
+                    SetPowerPlanSettingOnSystemTo(MaxBatterySaving);
+                    break;
+                default:
+                    base.OnPowerEvent(powerStatus);
+                    break;
+            }
+            return true;
+        }
+
+        private void SetPowerPlanSettingOnSystemByConnectedPowerStatus()
+        {
+            switch(SystemInformation.PowerStatus.PowerLineStatus)
+            {
+                case PowerLineStatus.Online:
+                    SetPowerPlanSettingOnSystemTo(MaxPerformance);
+                    break;
+                case PowerLineStatus.Offline:
+                    SetPowerPlanSettingOnSystemTo(Balanced);
+                    break;
+                default:
+                    SetPowerPlanSettingOnSystemTo(Balanced);
+                    break;
+            }
+        }
+
+        private void SetPowerPlanSettingOnSystemTo(string powerPlanSetting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
